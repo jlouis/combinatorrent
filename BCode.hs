d0ed240c8b2f09f825d073729f6663b09a86f529
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-11-16 18:54:59 +0100

    Quick'n'dirty parser.

--- BCode.hs
+++ BCode.hs
@@ -10,6 +10,7 @@ where
 
 import Data.Maybe
 import qualified Data.Map as M
+import Text.ParserCombinators.Parsec
 
 data BCode = BInt Int
            | BString String
@@ -26,9 +27,44 @@ encode (BArray arr) = "l" ++ concatMap encode arr ++ "e"
 encode (BDict mp) = "d" ++ concatMap encPair (M.toList mp) ++ "e"
     where encPair (k, v) = encode (BString k) ++ encode v
 
+
+parseInt :: GenParser Char st BCode
+parseInt = do
+  char 'i'
+  i <- many1 digit
+  char 'e'
+  return $ BInt $ read i
+
+parseString :: GenParser Char st BCode
+parseString = do
+  n <- many1 digit
+  let i = read n :: Int
+  s <- count i anyChar
+  return $ BString s
+
+parseList :: GenParser Char st BCode
+parseList = do
+  char 'l'
+  l <- many parseBCode
+  char 'e'
+  return $ BArray l
+
+parseDict :: GenParser Char st BCode
+parseDict = do char 'd'
+               l <- many parsePair
+               char 'e'
+               return $ BDict $ M.fromList l
+  where parsePair = do
+          (BString s) <- parseString
+          b <- parseBCode
+          return (s,b)
+
+parseBCode :: GenParser Char st BCode
+parseBCode = parseString <|> parseList <|> parseInt <|> parseDict
+
 -- Use parsec for this bastard
-decode :: String -> BCode
-decode = BString
+decode :: String -> Either ParseError BCode
+decode = parse parseBCode "(unknown)"
 
 
 {- Simple search function over BCoded data structures, general case. In practice, we
