8ba94e221ab9ffaf450498f2ceb5e08354f9690f
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-11-17 03:30:15 +0100

    Implement a waiting primitive so we only close when all forked processes close.

--- Main.hs
+++ Main.hs
@@ -1,8 +1,34 @@
 module Main (main)
 where
 
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Exception
+import Data.Maybe
 import System.Environment
+import System.IO.Unsafe
 import qualified BCode
+import qualified TrackerP
+
+children :: MVar [MVar ()]
+children = unsafePerformIO (newMVar [])
+
+waitForChildren :: IO ()
+waitForChildren = do
+  cs <- takeMVar children
+  case cs of
+    [] -> return ()
+    m:ms -> do
+            putMVar children ms
+            takeMVar m
+            waitForChildren
+
+forkChild :: IO () -> IO ThreadId
+forkChild io = do
+  mvar <- newEmptyMVar
+  childs <- takeMVar children
+  putMVar children (mvar:childs)
+  forkIO (io `finally` putMVar mvar ())
 
 main :: IO ()
 main = do
@@ -11,4 +37,7 @@ main = do
   let bcoded = BCode.decode torrent
   case bcoded of
     Left pe -> print pe
-    Right bc -> putStr $ BCode.prettyPrint bc
+    Right bc -> do peerC <- newChan
+                   taskC <- newChan
+                   forkChild  (TrackerP.spawn (fromJust (BCode.announce bc)) peerC taskC)
+                   waitForChildren
