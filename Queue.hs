5daaf23cc3e5094b79164f246d7219909baceed1
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-12-15 00:24:56 +0100

    Track some simple functional queues in Git.

---
+++ Queue.hs
@@ -0,0 +1,26 @@
+-- | Simple Functional queues based on a double list. This usually achieves good amortized bounds
+module Queue (Queue,
+              empty,
+              isEmpty,
+              push,
+              pop )
+where
+
+data Queue a = Queue [a] [a]
+
+empty :: Queue a
+empty = Queue [] []
+
+isEmpty :: Queue a -> Bool
+isEmpty (Queue [] []) = True
+isEmpty _             = False
+
+push :: Queue a -> a -> Queue a
+push (Queue front back) e = Queue front (e : back)
+
+
+pop :: Queue a -> Maybe (a, Queue a)
+pop (Queue []       [])   = Nothing
+pop (Queue (e : es) back) = Just (e, Queue es back)
+pop (Queue [] back)       = pop (Queue (reverse back) [])
+
