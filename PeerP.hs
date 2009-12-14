b040632b17e7c8b254fd9cdd57e0bbe773752bfd
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-12-15 00:25:09 +0100

    Implement a first version of sender process.

--- PeerP.hs
+++ PeerP.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString.Lazy as B
 import System.IO
 
 import WireProtocol
+import Queue
 
 -- | The raw sender process, it does nothing but send out what it syncs on.
 senderP :: Handle -> Channel (Maybe Message) -> IO ()
@@ -18,11 +19,26 @@ senderP sock ch = lp
                                 B.hPut sock bs
                                 lp
 
-data QueueState = QueueState {
-      messageQueueSlow :: [Message],
-      messageQueueFast :: [Message],
-      sendCh :: Channel (Maybe Message) }
+-- | sendQueue Process, simple version.
+--   TODO: Split into fast and slow.
+--   TODO: Make it possible to stop again.
+sendQueueP :: Channel Message -> Channel (Maybe Message) -> IO ()
+sendQueueP inC outC = lp empty
+  where lp eventQ =
+            do eq <- if isEmpty eventQ
+                       then sync $ queueEvent eventQ
+                       else sync $ choose [queueEvent eventQ, sendEvent eventQ]
+               lp eq
+        queueEvent q = wrap (receive inC (const True))
+                         (\msg -> return $ push q msg)
+        sendEvent q =
+            let Just (e, r) = pop q
+            in wrap (transmit outC $ Just e)
+                 (const $ return r)
 
-
-
-
+sendP :: Handle -> IO (Channel Message)
+sendP handle = do inC <- channel
+                  outC <- channel
+                  spawn $ senderP handle outC
+                  spawn $ sendQueueP inC outC
+                  return inC
