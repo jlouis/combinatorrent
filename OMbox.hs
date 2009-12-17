8724f8267050ef34c0c6527950757b5695e1c23d
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-12-17 22:24:51 +0100

    Track overwritable mailboxes.

---
+++ OMbox.hs
@@ -0,0 +1,20 @@
+
+-- | Overwritable mailboxes.
+--   An overwritable mailbox is like a SampleMVar in Haskells basic low-level concurrency, but
+--   implemented as a CML primitive.
+module OMbox
+where
+
+import Control.Concurrent.CML
+
+new :: IO (Channel a, Channel a)
+new = do putterC <- channel
+         getterC <- channel
+         spawn $ lp putterC getterC Nothing
+         return (putterC, getterC)
+  where lp putCh getCh Nothing = (sync $ putEvent putCh) >>= lp putCh getCh
+        lp putCh getCh (Just v) =
+            (sync $ choose [putEvent putCh, getEvent getCh v]) >>= lp putCh getCh
+        putEvent c = wrap (receive c (const True))
+                      (\v -> return $ Just v)
+        getEvent c v = wrap (transmit c v) (const $ return Nothing)
