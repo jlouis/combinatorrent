4fbf0523e956658afd5e22a4402c71e1de172fa9
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-12-20 13:01:58 +0100

    Track code for the Piece Manager. At the moment it is only type signatures.

---
+++ PieceMgrP.hs
@@ -0,0 +1,42 @@
+module PieceMgrP
+where
+
+
+import ConsoleP
+import Torrent
+
+start :: LogChannel -> IO ()
+start = undefined
+
+
+----------------------------------------------------------------------
+
+
+data Block = Block { blockOffset :: Int, -- ^ offset of this block within the piece
+                     blockSize   :: Int  -- ^ size of this block within the piece
+                   }
+
+data PieceDB = PieceDB { pendingPiece :: [PieceNum] -- ^ Pieces currently pending download
+                       , inProgressPiece  :: [(PieceNum, [Block])] -- ^ Pieces in progress of being downloaded
+                       , donePiece :: [PieceNum] -- ^ Pieces that are done
+                       }
+
+blockPiece :: BlockSize -> PieceSize -> [Block]
+blockPiece blockSz pieceSize = build pieceSize 0 []
+  where build leftBytes os accum | leftBytes >= blockSz = build (leftBytes - blockSz)
+                                                                    (os + blockSz)
+                                                                    $ (Block os blockSz) : accum
+                                 | otherwise = reverse $ Block os leftBytes : accum
+
+
+-- | The call @grabBlocks n eligible db@ tries to pick off up to @n@ pieces from
+--   the @n@. In doing so, it will only consider pieces in @eligible@. It returns a
+--   pair @(blocks, db')@, where @blocks@ are the blocks it picked and @db'@ is the resulting
+--   db with these blocks removed.
+grabBlocks :: Int -> [PieceNum] -> PieceDB -> ([(PieceNum, [Block])], PieceDB)
+grabBlocks = undefined
+
+-- | Put back a set of blocks into the piece database. This call is used whenever we loose a
+--   peer as his share of blocks are to be added back for downloading.
+putBlock :: [(PieceNum, [Block])] -> PieceDB -> PieceDB
+putBlock = undefined
