821b26d5142fb14a51326d1e4318d8727cd4044a
Alex Mason <axman6@gmail.com>
2010-01-08 01:12:45 +1100

    Work on using cereal for WireProtocol
     * The encoding and parsing are finished (I think)
     * Still need to implements receiveHeader

--- src/WireProtocol-cereal.hs
+++ src/WireProtocol-cereal.hs
@@ -7,9 +7,17 @@ import Control.Monad
 import Data.Monoid
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
-import Data.ByteString.Builder
-import Data.ByteString.Parser
+
+import Data.Serialize
+import Data.Serialize.Put
+import Data.Serialize.Get
+
+-- REMOVE LATER
+-- import Data.ByteString.Builder
+-- import Data.ByteString.Parser
+
 import Data.Word
+import Data.Char
 import System.IO
 
 import ConsoleP
@@ -17,15 +25,15 @@ import Torrent
 
 ------------------------------------------------------------
 
-type BitField = L.ByteString
-
+type BitField    = L.ByteString
 type PieceLength = Int
+
 data Message = KeepAlive
              | Choke
              | Unchoke
              | Interested
              | NotInterested
-             | Have PieceNum
+             | Have PieceNum -- Int
              | BitField BitField
              | Request PieceNum Block
              | Piece PieceNum Int B.ByteString
@@ -33,6 +41,10 @@ data Message = KeepAlive
              | Port Integer
   deriving (Eq, Show)
 
+
+data HandShake = HandShake 
+                  String -- Protocol Header
+                  Word64 -- Extension Bias
 
 -- | The Protocol header for the Peer Wire Protocol
 protocolHeader :: String
@@ -44,111 +56,111 @@ extensionBasis = 0
 extensionFast :: Word64
 extensionFast = 4
 
+p8 :: Word8 -> Put
+p8 = putWord8
 
--- Seriously consider HCodecs as a dependency
-encodeMsg :: Message -> Builder
-encodeMsg KeepAlive       = empty
-encodeMsg Choke           = singleton 0
-encodeMsg Unchoke         = singleton 1
-encodeMsg Interested      = singleton 2
-encodeMsg NotInterested   = singleton 3
-encodeMsg (Have pn)       =
-    mconcat [singleton 4,
-             putW32be pn
-            ]
-encodeMsg (BitField bf)   =
-    mconcat [singleton 5,
-             fromLazyByteString bf
-            ]
-encodeMsg (Request pn (Block os sz)) =
-    mconcat [singleton 6,
-             putW32be pn,
-             putW32be os,
-             putW32be sz
-            ]
-encodeMsg (Piece pn os c) =
-    mconcat [singleton 7,
-             putW32be pn,
-             putW32be os,
-             fromByteString c
-            ]
-encodeMsg (Cancel pn (Block os sz))  =
-    mconcat [singleton 8,
-             putW32be pn,
-             putW32be os,
-             putW32be sz
-            ]
-encodeMsg (Port p)        =
-    mconcat [singleton 9,
-             putW32be p
-            ]
+p32be :: Integral a => a -> Put
+p32be = putWord32be . fromIntegral
 
--- Helper function to make code above clearer
-putW32be :: Integral a => a -> Builder
-putW32be = putWord32be . fromIntegral
+instance Serialize Message where
+    put KeepAlive       = return ()
+    put Choke           = p8 0
+    put Unchoke         = p8 1
+    put Interested      = p8 2
+    put NotInterested   = p8 3
+    put (Have pn)       = p8 4 *> p32be pn
+    put (BitField bf)   = p8 5 *> putLazyByteString bf
+    put (Request pn (Block os sz))
+                        = p8 6 *> mapM_ p32be [pn,os,sz]
+    put (Piece pn os c) = p8 7 *> mapM_ p32be [pn,os] *> putByteString c
+    put (Cancel pn (Block os sz))
+                        = p8 8 *> mapM_ p32be [pn,os,sz]
+    put (Port p)        = p8 9 *> (putWord16be . fromIntegral $ p)
+    
+    get =  getKA      <|> getChoke
+       <|> getUnchoke <|> getIntr
+       <|> getNI      <|> getHave
+       <|> getBF      <|> getReq
+       <|> getPiece   <|> getCancel
+       <|> getPort
 
-decodeMsg :: Parser Message
-decodeMsg =
-    do m <- getWord8
-       case m of
-         0 -> return Choke
-         1 -> return Unchoke
-         2 -> return Interested
-         3 -> return NotInterested
-         4 -> Have     <$> gw32
-         5 -> BitField <$> getRemainingLazyByteString
-         6 -> Request  <$> gw32 <*> (Block <$> gw32 <*> gw32)
-         7 -> Piece    <$> gw32 <*> gw32 <*> (remaining >>= getByteString . fromIntegral) --getRemainingLazyByteString)
-         8 -> Cancel   <$> gw32 <*> (Block <$> gw32 <*> gw32)
-         9 -> Port . fromIntegral <$> getWord16be
-         _ -> fail "Incorrect message parse"
-  where gw32 = fromIntegral <$> getWord32be
+getChoke   = byte 0 *> return Choke
+getUnchoke = byte 1 *> return Unchoke
+getIntr    = byte 2 *> return Interested
+getNI      = byte 3 *> return NotInterested
+getHave    = byte 4 *> (Have <$> gw32)
+getBF      = byte 5 *> (BitField <$> (remaining >>= getLazyByteString . fromIntegral))
+getReq     = byte 6 *> (Request  <$> gw32 <*> (Block <$> gw32 <*> gw32))
+getPiece   = byte 7 *> (Piece    <$> gw32
+                                 <*> gw32
+                                 <*> (remaining >>= getByteString))
+getCancel  = byte 8 *> (Cancel   <$> gw32 <*> (Block <$> gw32 <*> gw32))
+getPort    = byte 9 *> (Port . fromIntegral <$> getWord16be)
+getKA      = do
+    empty <- isEmpty
+    if empty
+        then return KeepAlive
+        else fail "Non empty message - not a KeepAlive"
 
--- | encode a message for transmit on a socket
-encode :: Message -> L.ByteString
-encode m = toLazyByteString $ mconcat [putW32be sz, bld]
-  where bld = encodeMsg m
-        sz = L.length $ toLazyByteString bld -- Suboptimal, but works :)
+gw32 :: Integral a => Get a
+gw32 = fromIntegral <$> getWord32be
+byte :: Word8 -> Get Word8
+byte w = do
+    x <- lookAhead getWord8
+    if x == w
+        then getWord8
+        else fail $ "Expected byte: '" ++ show w ++ "' got: '" ++ show x ++ "'"
+
+
+-- | Protocol handshake code. This encodes the protocol handshake part
+-- protocolHandshake :: L.ByteString
+-- protocolHandshake = toLazyByteString $ mconcat [putWord8 $ fromIntegral sz,
+--                                                 putString protocolHeader,
+--                                                 putWord64be caps]
+--   where caps = extensionBasis
+--         sz = length protocolHeader
 
 
 -- | Protocol handshake code. This encodes the protocol handshake part
 protocolHandshake :: L.ByteString
-protocolHandshake = toLazyByteString $ mconcat [putWord8 $ fromIntegral sz,
-                                                putString protocolHeader,
-                                                putWord64be caps]
-  where caps = extensionBasis
-        sz = length protocolHeader
+protocolHandshake = L.fromChunks . map runPut $
+                    [putWord8 $ fromIntegral sz,
+                     mapM_ (putWord8 . fromIntegral . ord) protocolHeader,
+                     putWord64be extensionBasis]
+  where sz = length protocolHeader
 
+
+
+-- 
 -- | Receive the header parts from the other end
-receiveHeader :: Handle
-              -> Int
-              -> InfoHash
-              -> IO (Either String ([Capabilities], L.ByteString))
-receiveHeader h sz ih = parseHeader `fmap` L.hGet h sz
-    -- do handshake <- L.hGet h sz
-    --    return $ parseHeader handshake
-  where protocolHeaderSize = length protocolHeader
-        parseHeader = runParser parser
-        parser =
-            do hdSz <- getWord8
-               when (fromIntegral hdSz /= protocolHeaderSize) $ fail "Wrong header size"
-               protoString <- getString protocolHeaderSize
-               when (protoString /= protocolHeader) $ fail "Wrong protocol header"
-               caps <- getWord64be
-               ihR   <- getLazyByteString 20
-               when (ihR /= ih) $ fail "Wrong InfoHash"
-               pid <- getLazyByteString 20
-               return (decodeCapabilities caps, pid)
+-- receiveHeader :: Handle
+--               -> Int
+--               -> InfoHash
+--               -> IO (Either String ([Capabilities], L.ByteString))
+-- receiveHeader h sz ih = parseHeader `fmap` L.hGet h sz
+--     -- do handshake <- L.hGet h sz
+--     --    return $ parseHeader handshake
+--   where protocolHeaderSize = length protocolHeader
+--         parseHeader = runParser parser
+--         parser =
+--             do hdSz <- getWord8
+--                when (fromIntegral hdSz /= protocolHeaderSize) $ fail "Wrong header size"
+--                protoString <- getString protocolHeaderSize
+--                when (protoString /= protocolHeader) $ fail "Wrong protocol header"
+--                caps <- getWord64be
+--                ihR   <- getLazyByteString 20
+--                when (ihR /= ih) $ fail "Wrong InfoHash"
+--                pid <- getLazyByteString 20
+--                return (decodeCapabilities caps, pid)
 
 data Capabilities = Fast
-
+-- 
 decodeCapabilities :: Word64 -> [Capabilities]
 decodeCapabilities _ = []
 
 -- | Initiate a handshake on a socket
-initiateHandshake :: LogChannel ->
-                     Handle -> PeerId -> InfoHash -> IO (Either String ([Capabilities],
-                                                                        L.ByteString))
+initiateHandshake :: LogChannel -> Handle -> PeerId -> InfoHash
+                  -> IO (Either String ([Capabilities], L.ByteString))
 initiateHandshake logC handle peerid infohash = do
     logMsg logC "Sending off handshake message"
     L.hPut handle msg
@@ -159,13 +171,29 @@ initiateHandshake logC handle peerid infohash = do
                                           fromLazyByteString infohash,
                                           putString peerid]
         sz = fromIntegral (L.length msg)
-
--- TESTS
+-- 
+-- -- TESTS
 testDecodeEncodeProp1 :: Message -> Bool
 testDecodeEncodeProp1 m =
-    let encoded = toLazyByteString $ encodeMsg m
-        decoded = runParser decodeMsg encoded
+    let encoded = encode m
+        decoded = decode encoded
     in case decoded of
          Left _ -> False
          Right m' -> m == m'
+
+-- A fairly comprehensive test to use:
+-- Prelude.map testDecodeEncodeProp1 
+--  [KeepAlive,
+--   Choke,
+--   Unchoke,
+--   Interested,
+--   NotInterested,
+--   Have 0,
+--   Have 1, =
+--   Have 1934729,
+--   BitField (Data.ByteString.Lazy.pack [1,2,3]),
+--   Request 123 (Block 4 7),
+--   Piece 5 7 (Data.ByteString.pack [1,2,3,4,5,6,7,8,9,0]),
+--   Cancel 5 (Block 6 7), Port 123]
+-- Currently returns all True
 
