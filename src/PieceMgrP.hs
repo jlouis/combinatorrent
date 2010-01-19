a8c937d2b439a3a24c11a526e5b85606051d976c
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-01-19 18:20:52 +0100

    Pick a Random Piece rather than the first one.

--- src/PieceMgrP.hs
+++ src/PieceMgrP.hs
@@ -19,6 +19,8 @@ import qualified Data.Set as S
 
 import Prelude hiding (log)
 
+import System.Random
+
 import Logging
 import FSP hiding (start, fspCh)
 import Supervisor
@@ -223,7 +225,8 @@ grabBlocks' k eligible = tryGrabProgress k eligible []
 	pending <- gets pendingPieces
         case ps `intersect` pending of
           []    -> return captured -- No (more) pieces to download, return
-          (h:_) -> do
+          ls    -> do
+	      h <- pickRandom ls
 	      infMap <- gets infoMap
 	      inProg <- gets inProgress
               blockList <- createBlock h
@@ -232,6 +235,9 @@ grabBlocks' k eligible = tryGrabProgress k eligible []
               modify (\db -> db { pendingPieces = pendingPieces db \\ [h],
                                   inProgress    = M.insert h ipp inProg })
 	      tryGrabProgress n ps captured
+    pickRandom pieces = do
+	n <- liftIO $ getStdRandom (\gen -> randomR (0, length pieces - 1) gen)
+	return $ pieces !! n
     createBlock :: Int -> PieceMgrProcess [Block]
     createBlock pn =
 	let cBlock = blockPiece
