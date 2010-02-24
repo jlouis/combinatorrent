69b9b4ec67f1ab607ad5dedd45cba4e4fbc5fc60
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-02-24 14:20:32 +0100

    Change to QuickCheck2 as the framework.

--- src/Test.hs
+++ src/Test.hs
@@ -1,8 +1,12 @@
+{- The Test module provides an interface to test-framework. It is called when --tests
+ - is supplied on the command line. This file only gathers together various test suites
+ - from all over the rest of the code and then executes them via test-framework.
+ -}
 module Test (runTests) where
 
 import System.Environment ( getArgs )
 import Test.Framework
-import Test.Framework.Providers.QuickCheck
+import Test.Framework.Providers.QuickCheck2
 
 import qualified Protocol.BCode (testSuite)
 import qualified Protocol.Wire  (testSuite)
