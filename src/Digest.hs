06340aca4d2173cf7baabda7b56f095ce6c1473b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-02-26 14:54:53 +0100

    Try hopenssl.

--- src/Digest.hs
+++ src/Digest.hs
@@ -5,25 +5,16 @@ module Digest
   )
 where
 
-import Control.Concurrent
+import Data.Char
 
 import qualified Data.ByteString.Lazy as L
-import OpenSSL
-import qualified OpenSSL.EVP.Digest as D
+import qualified OpenSSL.Digest as SSL hiding (Digest)
 
 -- Consider newtyping this
 type Digest = String
 
-sha1Digest :: IO D.Digest
-sha1Digest = do dig <- D.getDigestByName "SHA1"
-                case dig of
-                    Nothing -> fail "No such digest, SHA1"
-                    Just d -> return d
-
 digest :: L.ByteString -> IO Digest
-digest bs =
-  runInBoundThread $ -- I don't think this is needed strictly
-    withOpenSSL $
-        do sha1 <- sha1Digest
-           return $ D.digestLBS sha1 bs
+digest bs = do
+    upack <- SSL.digest SSL.SHA1 $ L.unpack bs
+    return $ map (chr . fromIntegral) upack
 
