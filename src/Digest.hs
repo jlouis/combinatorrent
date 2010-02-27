80250d678840ca5786806fd0dd1daeee2d197255
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-02-27 13:53:27 +0100

    Allow ByteStrings to be digested.
    This not only improves performance, but plugs the memory leak
    effectively.

--- src/Digest.hs
+++ src/Digest.hs
@@ -6,15 +6,33 @@ module Digest
 where
 
 import Data.Char
+import Data.Word
+import Control.Monad.State
 
+import Foreign.Ptr
+import qualified Data.ByteString as B
+import Data.ByteString.Unsafe
 import qualified Data.ByteString.Lazy as L
-import qualified OpenSSL.Digest as SSL hiding (Digest)
+import qualified OpenSSL.Digest as SSL
 
 -- Consider newtyping this
 type Digest = String
 
 digest :: L.ByteString -> IO Digest
 digest bs = do
-    upack <- SSL.digest SSL.SHA1 $ L.unpack bs
+    upack <- digestLBS SSL.SHA1 bs
     return $ map (chr . fromIntegral) upack
 
+digestLBS :: SSL.MessageDigest -> L.ByteString -> IO [Word8]
+digestLBS mdType xs =
+  SSL.mkDigest mdType $ evalStateT (updateLBS xs >> SSL.final)
+
+updateBS :: B.ByteString -> SSL.Digest ()
+updateBS bs = do
+    SSL.DST ctx <- get
+    liftIO $ unsafeUseAsCStringLen bs $
+            \(ptr, len) -> SSL.digestUpdate ctx (castPtr ptr) (fromIntegral len)
+    return ()
+
+updateLBS :: L.ByteString -> SSL.Digest ()
+updateLBS lbs = mapM_ updateBS $ L.toChunks lbs
