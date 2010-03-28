39a7d63b55901a61769a3163c8a941e53ac16b93
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-03-28 20:33:37 +0200

    Add plotting support via sparklines.

---
+++ tools/visualize_stats.R
@@ -0,0 +1,35 @@
+#R sparklines
+sparkline<-function(ydata=rnorm(100,500,50),width=1.5,height=0.5,sigfigs=4) {
+
+# ydata = vector of data to be plotted
+# width = width of sparlkline in inches, including text
+# height = height of sparkline in inches
+# sigfigs = number of significant figures to round min, max, and last values to
+
+        temppar<-par # store default graphics parameters
+        par(mai=c(0.0,0.05,0.0,0.05),fin=c(width,height))
+        len<-length(ydata) # determine the length of the data set
+        ymin<-min(ydata) # determine the minimum
+        tmin<-which.min(ydata) # and its index
+        ymax<-max(ydata) # determine the maximum
+        tmax<-which.max(ydata) # and its index
+        yfin<-signif(ydata[len],sigfigs) #determine most recent data point
+        plotrange=c(ymin-0.3*(ymax-ymin),ymax+0.3*(ymax-ymin)) # define plot range to leave enough room for min and max circles and text
+        plot(x=1:len,y=ydata,type="l",xlim=c(1,len*1.5),ylim=plotrange,col="gray",lwd=0.5,ann=FALSE,axes=FALSE) # plot sparkline
+        points(x=c(tmin,tmax),y=c(ymin,ymax),pch=19,col=c("red","blue"),cex=0.5) # plot min and max points
+        text(x=len,y=ymin,labels=signif(ymin,sigfigs),cex=0.5,pos=4,col="red") # show minimum value
+        text(x=len,y=ymax,labels=signif(ymax,sigfigs),cex=0.5,pos=4,col="blue") # show maximum value
+        text(x=len,y=(ymin+ymax)/2,labels=yfin,cex=0.5,pos=4) # show most recent value
+        par(temppar) # restore graphics defaults
+}
+
+stat <- read.csv("stat_db.csv", header=TRUE, sep="\t")
+attach(stat)
+max_space <- rbind(MaxBytesUsed)
+peak_alloc <- rbind(PeakMegabytesAlloc)
+png(filename="MaxBytesUsed.png", width=125, height=39)
+sparkline(max_space / (1024*1024), width=1.5, height=0.50)
+dev.off()
+png(filename="PeakMegabytes.png", width=125, height=39)
+sparkline(peak_alloc, width=1.5, height=0.5)
+dev.off()
